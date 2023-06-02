419f2330baa9e13309a0bc8ce47b8e62b9343669
devap247@gmail.com <devapp247@gmail.com>
2023-06-02 19:08:34 +0700

    Create bt_tgc.c
    add new file

---
+++ bt_tgc.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+   int n,i,j;
+
+   n = 10;   
+
+   for(i = 1; i <= n; i++) {
+      for(j = 1; j <= n-i; j++)
+         printf(" ");
+
+      for(j = 1; j <= i; j++)
+         printf("* ");
+
+      printf("\n");
+   }
+   return 1;
+}
